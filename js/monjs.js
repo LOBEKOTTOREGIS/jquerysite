28a6056e8b4f354468c5ffacfc663d3d5484549e
LOBEKOTTOREGIS <rlobe@yahoo.fr>
2021-11-08 15:00:50 +0100

    ajout de projet

---
+++ js/monjs.js
@@ -0,0 +1,18 @@
+
+$("#main-content").load("accueil.html");
+
+$("#contact-page").click(function(){
+	$("#main-content").load("contact.html");
+});
+
+$("#about-page").click(function(){
+	$("#main-content").load("about.html");
+});
+
+$("#signin-page").click(function(){
+	$("#main-content").load("signin.html");
+});
+
+$("#signup-page").click(function(){
+	$("#main-content").load("signup.html");
+});
